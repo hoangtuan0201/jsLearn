12bffe6cd29a21b27c8b71c87f996fc4bbdd6f50
hoangtuan0201 <hoangtuan0796991061@gmail.com>
2024-08-03 20:25:07 +0700

    array

---
+++ 11-arrays-and-loop/toDoList.js
@@ -0,0 +1,8 @@
+const toDoList = [];
+function addTask() {
+    const taskInput = document.querySelector('.input-todo');
+    const taskName = taskInput.value;
+    toDoList.push(taskName);
+    console.log(toDoList);
+    taskInput.value = ''
+}
