68b4ab575e4839fef2c103dc68819eddb448dbd1
hoangtuan0201 <hoangtuan0796991061@gmail.com>
2024-08-06 20:27:38 +0700

    to do list vip pro cua tuan

--- 11-arrays-and-loop/toDoList.js
+++ 11-arrays-and-loop/toDoList.js
@@ -1,4 +1,13 @@
-const toDoList = [];
+const toDoList = [
+    {
+        name:'make dinner',
+        dueDate: '2023-6-8'
+    },
+    {
+        name: 'go to the gym',
+        dueDate: '2023-6-10'
+    }
+];
     renderTodoList();
 
 
@@ -7,7 +16,18 @@ const toDoList = [];
 function renderTodoList() {
     let todoListHTML = '';
     for (let i in toDoList) {
-        const html = `<p>${toDoList[i]}</p>`
+        // Tạo HTML cho từng task ứng với thông tin của task
+        // const name = toDoList[i].name;
+        const { name } = toDoList[i]; //destructuring property name to variable
+        // const dueDate = toDoList[i].dueDate;
+        const { dueDate } = toDoList[i]; //destructuring property dueDate to variable
+        const html = `<p>
+                                ${name} at ${dueDate} 
+                                <button class="delete-button" onclick="
+                                    toDoList.splice(${i}, 1); 
+                                    renderTodoList();
+                                ">Delete</button>
+                              </p>`
         todoListHTML += html;
     }
     document.querySelector('.js-todo-list').innerHTML = todoListHTML;
@@ -16,9 +36,18 @@ function renderTodoList() {
 
 function addTask() {
     const taskInput = document.querySelector('.input-todo');
-    const taskName = taskInput.value;
-    toDoList.push(taskName);
-    console.log(toDoList);
-    taskInput.value = ''
-    renderTodoList();
+    const dateInput = document.querySelector('.input-date');
+
+    if (!taskInput.value)  {
+        alert('Please select a task');
+    }else {
+        toDoList.push({
+            name:taskInput.value,
+            dueDate:dateInput.value
+        });
+
+        console.log(toDoList);
+        taskInput.value = '' // dòng này để sau mỗi lần thêm thì input sẽ trống
+        renderTodoList();
+    }
 }
